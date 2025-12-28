23d70d460866235772f0bcc639bb628559608684
Nicolas Bellomo <nbellomo506@gmail.com>
2025-12-28 12:24:08 +0100

    edit main

--- detector/main.py
+++ detector/main.py
@@ -2,15 +2,4 @@ import random_forest
 
 def main():
     # Load dataset
-    data = random_forest.load_data('data.csv')
-    
-    # Preprocess data
-    X_train, X_test, y_train, y_test = random_forest.preprocess_data(data)
-    
-    # Train model
-    model = random_forest.train_model(X_train, y_train)
-    
-    # Evaluate model
-    accuracy = random_forest.evaluate_model(model, X_test, y_test)
-    
-    print(f'Model Accuracy: {accuracy:.2f}%')
+    print("Loading dataset...")
